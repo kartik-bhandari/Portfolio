4befac07b39bc70cb5a3213f5f218e23af3f8bec
kartik <kumarkartikeybhandari@gmail.com>
2024-05-29 18:18:31 +0530

    size update 5

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
   
   return (
     <section className='relative w-full h-screen mx-auto'>
-      <div className='h-4/5'>
+      <div className='h-4/5 sm:h-full'>
          <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
